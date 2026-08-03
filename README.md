2d0a3e017a1df6299c3ff81920295023b3e3d5e2
Abhishek Kumbhar <kumbhara976@gmail.com>
2026-08-03 15:57:45 +0530

    updated code, style and js

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 - 🕵🏿‍♂️ Continuously enhancing my expertise in Full Stack Web Development, Machine Learning, and Data Structures & Algorithms using Java.
 - 🚴‍♂️ Reach me at: https://www.linkedin.com/in/abhishek-kumbhar-323129214/
 - 🌌 Portfolio: https://codolio.com/profile/avk19
+
+<!-- | Element        | Color     |
+| -------------- | --------- |
+| Background     | `#0D1117` |
+| Navbar         | `#161B22` |
+| Cards          | `#21262D` |
+| Border         | `#30363D` |
+| Primary Accent | `#58A6FF` |
+| Text           | `#F0F6FC` |
+| Secondary Text | `#8B949E` | -->
--- index.js
+++ index.js
@@ -1,5 +1,131 @@
-const resumeBtn = document.getElementById("resumeBtn");
+const homeHTML = `
+<h1>
+          Hi, I'm Abhishek!<br />
+          Software Engineer
+        </h1>
 
-resumeBtn.addEventListener("click", () => {
-  window.open("resume.pdf", "_blank");
+        <p>
+          Software Engineer with 2 years of experience in Java Backend
+          Development using Spring Boot. Passionate about building scalable
+          applications, solving Data Structures & Algorithms, and continuously
+          improving my Full Stack Development and Machine Learning skills.
+        </p>
+
+        <a href="resume/abhishek_kumbhar.pdf" target="_blank" id="resumeButton">
+          ⬇ View Resume
+        </a>    
+`;
+
+const skillsHTML = `
+  <h1>Skills</h1>
+
+      <h3>Languages: Java, JavaScript, C++, SQL</h3>
+
+      <h3>Frameworks: Spring Boot, React, Node.JS</h3>
+
+      <h3>Databases: MySQL, MongoDB</h3>
+
+      <h3>Tools: Intellij, Eclips, Spring suite, Git, Docker, Postman, VS Code</h3>`;
+
+const codingHTML = `
+ <h1>Coding Profiles</h1>
+    <a href="https://github.com/avk1996" target="_blank"> GitHub </a><br/>
+    <a href="https://leetcode.com/kumbhara976" target="_blank">LeetCode</a><br/>
+    <a href="https://www.hackerrank.com/kumbhara976" target="_blank">HackerRank</a><br/>
+`;
+
+const projectsHTML = `
+  <h1>Projects</h1>
+
+  <article>
+        <h3>Quiz Management System</h3>
+
+        <p>Description of your project.</p>
+
+        <a href="#">GitHub</a>
+
+        <a href="#">Live Demo</a>
+      </article>
+
+      <article>
+        <h3>Train Reservation System</h3>
+
+        <p>Description of your project.</p>
+
+        <a href="#">GitHub</a>
+      </article>
+`;
+
+const contactsHTML = `
+ <h1>Contact</h1>
+
+      <p>
+        <strong>Email:</strong>
+
+        <a href="kumbhara976@gmail.com"> kumbhara976@gmail.com </a>
+      </p>
+
+      <p>
+        <strong>LinkedIn:</strong>
+
+        <a
+          href="https://linkedin.com/in/abhishek-kumbhar-323129214"
+          target="_blank"
+        >
+          LinkedIn Profile
+        </a>
+      </p>
+
+      <p>
+        <strong>GitHub:</strong>
+
+        <a href="https://github.com/avk1996" target="_blank">
+          GitHub Profile
+        </a>
+      </p>`;
+
+const home = document.getElementById("homeLink");
+const skill = document.getElementById("skillsLink");
+const coding = document.getElementById("codingLink");
+const project = document.getElementById("projectsLink");
+const contact = document.getElementById("contactLink");
+
+const content = document.getElementById("content");
+
+changeContent(homeHTML);
+
+home.addEventListener("click", (event) => {
+  event.preventDefault();
+  changeContent(homeHTML);
 });
+
+skill.addEventListener("click", (event) => {
+  event.preventDefault();
+  changeContent(skillsHTML);
+});
+
+coding.addEventListener("click", (event) => {
+  event.preventDefault();
+  changeContent(codingHTML);
+});
+
+project.addEventListener("click", (event) => {
+  event.preventDefault();
+  // content.innerHTML = projectsHTML;
+  changeContent(homeHTML);
+});
+
+contact.addEventListener("click", (event) => {
+  event.preventDefault();
+  changeContent(contactsHTML);
+});
+
+function changeContent(html) {
+  content.classList.add("fade");
+
+  setTimeout(() => {
+    content.innerHTML = html;
+
+    content.classList.remove("fade");
+  }, 300);
+}
